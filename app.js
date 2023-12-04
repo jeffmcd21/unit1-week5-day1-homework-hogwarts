05b5779b43ea284a0697d31c87937b8fd9fd7365
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-04 09:03:43 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -5,16 +5,43 @@
 
 $(() => {
 	// put jQuery in here
-    
+
+ // ----- ***** ----- Year 1 ----- ***** ----- //
+
     //Query for your div with the id of container and set it to a variable named $container
     const $container = $("#container");
-    console.log($container)
+        //console.log($container)
 
     //Create an <h1> element and set it to a variable called $h1
     const $h1 = $('<h1>Hogwarts</h1>');
-    console.log($h1)
+        //console.log($h1)
 
     //Append your $h1 to your $container
     $container.append($h1);
+
+// ----- ***** ----- Year 2 ----- ***** ----- //
+
+    //h2 element with your name
+    const $h2 = $('<h2>Jeff</h2>');
+    $container.append($h2)
+        //console.log($h2)
+
+
+    // h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
+    const $h3 = $('<h3>Ravenclaw</h3>')
+    $container.append($h3)
+        //console.log($h3)
+
+    // h4 element with your pet's name
+    // this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+    const $h4 = $('<h4>Marley</h4>');
+    // Add a class with the pet type (dog)
+    $h4.addClass('dog');
+    $container.append($h4);
+
+    // h4 element with your wand 
+    const $h4Two = $('<h4>Holly Wand with Unicorn Hair Core</h4>');
+    $container.append($h4Two);
+
 });
 
