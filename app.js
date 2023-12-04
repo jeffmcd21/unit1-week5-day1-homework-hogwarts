aa48eb9635383129c64a7ca5fa754661831ac3c3
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-04 06:37:14 -0500

    Commit 1 - I added my first element and it was fun

--- app.js
+++ app.js
@@ -3,3 +3,18 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 
+$(() => {
+	// put jQuery in here
+    
+    //Query for your div with the id of container and set it to a variable named $container
+    const $container = $("#container");
+    console.log($container)
+
+    //Create an <h1> element and set it to a variable called $h1
+    const $h1 = $('<h1>Hogwarts</h1>');
+    console.log($h1)
+
+    //Append your $h1 to your $container
+    $container.append($h1);
+});
+
