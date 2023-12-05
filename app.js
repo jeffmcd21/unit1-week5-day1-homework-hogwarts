15ab1321405366b1b410ff8574917c2ec54b5a09
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-04 23:37:55 -0500

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ $(() => {
         //console.log($container)
 
     //Create an <h1> element and set it to a variable called $h1
-    const $h1 = $('<h1>Hogwarts</h1>');
+    const $h1 = $("<h1>Hogwarts</h1>");
         //console.log($h1)
 
     //Append your $h1 to your $container
@@ -22,31 +22,31 @@ $(() => {
 // ----- ***** ----- Year 2 ----- ***** ----- //
 
     //h2 element with your name
-    const $h2 = $('<h2>Jeff</h2>');
+    const $h2 = $("<h2>Jeff</h2>");
     $container.append($h2)
         //console.log($h2)
 
 
     // h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
-    const $h3 = $('<h3>Ravenclaw</h3>')
+    const $h3 = $("<h3>Ravenclaw</h3>");
     $container.append($h3)
         //console.log($h3)
 
     // h4 element with your pet's name
     // this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
-    const $h4 = $('<h4>Marley</h4>');
+    const $h4 = $("<h4>Marley</h4>");
     // Add a class with the pet type (dog)
-    $h4.addClass('dog');
+    $h4.addClass("dog");
     $container.append($h4);
 
     // h4 element with your wand 
-    const $h4Two = $('<h4>Holly Wand with Unicorn Hair Core</h4>');
+    const $h4Two = $("<h4>Holly Wand with Unicorn Hair Core</h4>");
     $container.append($h4Two);
 
 // ----- ***** ----- Year 3 ----- ***** ----- //
 
 // create an unordered list with storage attribute set to "trunk"
-const $ul = $('<ul>', { storage: 'trunk' });
+const $ul = $("<ul>", { storage: "trunk" });
 
 // create list items for each item in the list
 const items = [
@@ -63,16 +63,48 @@ items.forEach(item => {
   if (typeof item === "string") {
     $ul.append($("<li>", { text: item }));
   } else {
-    $ul.append($('<li>', { text: item.text, class: item.class }));
+    $ul.append($("<li>", { text: item.text, class: item.class }));
   }
 });
 
 // append the list to the container
-$('#container').append($ul);
+$("#container").append($ul);
 
 
 // Remove list item bullet points
-$('ul').css('list-style', 'none');
+$("ul").css("list-style", "none");
 
+// ----- ***** ----- Year 4 ----- ***** ----- //
 
+    // Create table variable interpolate table structure and elements
+    const classTable = `
+    <table>
+        <tr>
+            <th>Day</th>
+            <th>Classes</th>
+        </tr>
+        <tr>
+            <td>Monday</td>
+            <td>Herbology</td>
+        </tr>
+        <tr>
+            <td>Tuesday</td>
+            <td>Divination</td>
+        </tr>
+        <tr>
+            <td>Wednesday</td>
+            <td>History of Magic</td>
+        </tr>
+        <tr>
+            <td>Thursday</td>
+            <td>Charms</td>
+        </tr>
+        <tr>
+            <td>Friday</td>
+            <td>Potions</td>
+        </tr>
+    </table>`
+    // Append header and table to div 
+    $container.append($("<h5>Spring 2017</h5>")).append($(classTable))
+    
 });
